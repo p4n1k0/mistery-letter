6247bdacfea73f91b8fe6514e8a9e4b0984ca0a3
p4n1k0 <gabre.tarick@gmail.com>
2024-11-29 16:04:22 -0300

    estilizacao

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const createBtn = document.getElementById('criar-carta');
 const textInput = document.getElementById('carta-texto');
 const letterCreated = document.getElementById('carta-gerada');
+const classes = {
+    style: ['newspaper1', 'newspaper2', 'magazine1', 'magazine2'],
+    size: ['medium', 'big', 'reallybig'],
+    rotation: ['rotateleft', 'rotateright'],
+    skew: ['skewleft', 'skewright'],
+};
 
 
 function createLetter() {
@@ -16,5 +22,39 @@ function createLetter() {
             letterCreated.appendChild(letter);
         }
     }
+    randonClass();
+    wordCounter();
 }
 createBtn.addEventListener('click', createLetter);
+
+function randonClass() {
+    const classes = {
+        style: ['newspaper', 'magazine1', 'magazine2'],
+        size: ['medium', 'big', 'reallybig'],
+        rotation: ['rotateleft', 'rotateright'],
+        skew: ['skewleft', 'skewright'],
+    };
+
+    const words = document.getElementsByTagName('span');
+    for (let i = 0; i < words.length; i += 1) {
+        for (const j in classes) {
+            const randomI = Math.floor(Math.random() * classes[j].length);
+            words[i].classList.add(classes[j][randomI]);
+        }
+    }
+}
+
+function reassignClasses(event) {
+    const clickedWord = event.target;
+    clickedWord.className = '';
+    for (const j in classes) {
+        const randomIndex = Math.floor(Math.random() * classes[j].length);
+        event.target.classList.add(classes[j][randomIndex]);
+    }
+}
+
+// Conta numero de palavras e mostra em um parágrafo
+function wordCounter() {
+    const wordCount = document.getElementById('carta-contador');
+    wordCount.innerText = `${letterCreated.children.length} PALAVRAS`
+}
